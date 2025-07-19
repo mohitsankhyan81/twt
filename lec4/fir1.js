b8fb5c1b7758588b1bb63b5806df35a49c0879a9
Mohit <mohitsankhyan253@gmail.com>
2025-07-19 17:55:40 +0530

    routing

--- lec4/fir1.js
+++ lec4/fir1.js
@@ -1,13 +1,34 @@
-//understand requir object 
+const http = require('http');
 
-const http=require('http');
+const server = http.createServer((req, res) => {
+  const url = req.url; // Get the URL from the request
 
-const server=http.createServer((req,res)=>{
-  console.log(req.url,req.method,req.headers);
+  // Set content type for all responses
+  res.setHeader('Content-Type', 'text/html');
+
+  //routing the code in local host;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Home Page</title></head>');
+    res.write('<body><h1>Welcome to Home</h1></body>');
+    res.write('</html>');
+    return res.end();
+  } 
+  else if (url === '/products') {
+    res.write('<html>');
+    res.write('<head><title>Products</title></head>');
+    res.write('<body><h1>Our Products</h1></body>');
+    res.write('</html>');
+    return res.end();
+  }
+    res.write('<html>');
+    res.write('<head><title>Not Found</title></head>');
+    res.write('<body><h1>404 - Page Not Found</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
-const port=2002;
-
-server.listen(port,()=>{
-  console.log("server is running properly");
-})
+const port = 2002;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
