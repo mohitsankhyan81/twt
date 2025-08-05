3751d9be67fdb7eed577ab4fafcfdbfd635e88b0
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-08-05 15:47:16 +0530

    commit

--- practice/app.js
+++ practice/app.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const app = express();
+const express=require('express');
 
-app.get("/", (req, res) => {
-  console.log("This is my first iteration", req.url, req.method);
-  res.send("<h1>My name is Mohit Sankhyan and I am from Kalka</h1>");
+const app=express();
+
+app.get("/",(req,res,next)=>{
+  console.log("this is my first itration",req.url,req.method);
+  res.send("<h1>My name is mohit Sankhyan and i am form kalka</h1>");
 });
 
-app.get("/submit", (req, res) => {
-  console.log("This is my second iteration", req.url, req.method);
-  res.send("<h1>Robin Singh is from Jammu and is not a good person. He is a weight for Earth.</h1>");
+app.get("/submit",(req,res,next)=>{
+  console.log("THis is my second iteration",req.url,req.method);
+  res.send("<h1>we are student of chitakar ans i love coding</h1>");
 });
 
-const port = 4000;
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+const port=4001;
+app.listen(port,()=>{
+  console.log(`server is http://localhost:${port}`);
+})
